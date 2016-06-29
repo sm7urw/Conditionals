1077f9b00e3206c3fd6290874aa7d3720bd56290
sm7urw <henrik@korslind.com>
2016-06-29 22:47:28 +0200

    first commit

---
+++ Conditionals.playground/Contents.swift
@@ -0,0 +1,79 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var isMyHouseOnFire: Bool = false
+
+var anotherBool = false
+
+if isMyHouseOnFire {
+
+    print ("Someone give me some water")
+
+} else {
+
+print ("Someone get som fire!")
+
+}
+
+var result = true == true
+result = true == false
+result = false == false
+
+var accountTotal = 9324
+var newBoat = 12000
+
+if accountTotal >= newBoat {
+    
+    print ("I will buy the boat!")
+    
+} else {
+    
+    print ("I cant afford to buy the boat!")
+    
+}
+
+
+var name = "Jack"
+
+if name == "Jack" {
+    
+    print ("Your name is Jack!")
+    
+} else {
+    
+    print ("Your name is not Jack")
+    
+}
+
+var numberA = 25
+var numberB = 40
+var numberC = 100
+var numberD = 50
+var finalNumber = 100
+
+if numberA == finalNumber {
+    
+    print (numberA)
+    
+} else if numberB == finalNumber {
+    
+    print (numberB)
+    
+} else if numberC == finalNumber{
+    
+    print (numberC)
+
+} else if numberD == finalNumber {
+    
+    print (numberD)
+    
+    
+} else {
+    
+    print ("Not final number")
+
+}
+
